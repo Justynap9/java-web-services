033ffca1a5acc8b841f7dc22c4e8b34613d7e9a1
Justynap9 <justyna948@gmail.com>
2022-06-16 21:15:13 +0200

    "add movie entity"

--- Project/MovieCatalogService/src/main/java/com/example/moviecatalogservice/models/Movie.java
+++ Project/MovieCatalogService/src/main/java/com/example/moviecatalogservice/models/Movie.java
@@ -1,4 +1,44 @@
 package com.example.moviecatalogservice.models;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "Movie")
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
