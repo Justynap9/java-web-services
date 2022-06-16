3024988de9d59006dd5ef94c6d713b0d74a33d41
Justynap9 <justyna948@gmail.com>
2022-06-16 22:16:21 +0200

    add entities in MovieCatalogModule

--- Project/MovieCatalogModule/src/main/java/com/example/moviecatalogservice/MovieCatalogModuleApplication.java
+++ Project/MovieCatalogModule/src/main/java/com/example/moviecatalogservice/MovieCatalogModuleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieCatalogServiceApplication {
+public class MovieCatalogModuleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MovieCatalogServiceApplication.class, args);
+        SpringApplication.run(MovieCatalogModuleApplication.class, args);
     }
 
 }
