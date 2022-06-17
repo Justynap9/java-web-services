7f4bc7cac1d748e4cb253567b53339f29f5e27be
Pawel <pawel.pechta@edu.uekat.pl>
2022-06-17 02:31:58 +0200

    feat: created ratings service

--- Project/RatingsDataModule/src/main/java/com/example/ratingsdatamodule/models/UserRating.java
+++ Project/RatingsDataModule/src/main/java/com/example/ratingsdatamodule/models/UserRating.java
@@ -1,4 +1,33 @@
 package com.example.ratingsdatamodule.models;
 
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Table;
+import java.util.List;
+
+@Table(name = "UserRating")
+@ToString
+@NoArgsConstructor
+
 public class UserRating {
+
+    private String userId;
+    private List<Rating> ratings;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public List<Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        this.ratings = ratings;
+    }
 }
